bebfbbe8be827a4a849046bda0159d71663a00f8
Mandanarz <mandy_923r@yahoo.com>
2020-07-21 09:11:50 +0200

    Style navigations and add X button to go to Document page

--- src/navigation/Nav.js
+++ src/navigation/Nav.js
@@ -1,26 +1,42 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter, Redirect } from "react-router-dom";
 import { getUrlFriendlyName } from "../utilities/urls";
+import "./Nav.css";
 
 class Nav extends React.Component {
-  getLinks = () => {
+  getParam = () => {
+    return this.props.location.pathname.split("/").pop();
+  };
+
+  getLinks = (param) => {
     if (this.props.data && Array.isArray(this.props.data)) {
       return this.props.data.map((item, key) => {
         const linkName = item && item.name ? getUrlFriendlyName(item.name) : "";
-
-        return (
-          <li key={key} className="nav-item active">
-            <Link className="nav-link" to={`/artboards/${linkName}`}>
-              {item.name}
-            </Link>
-          </li>
-        );
+        const className = param === linkName ? "selected" : "default";
+        if (param && param !== "") {
+          return (
+            <li key={key} className={`nav-item active ${className}`}>
+              <Link className="nav-link" to={`/artboards/${linkName}`}>
+                {item.name}
+              </Link>
+            </li>
+          );
+        } else {
+          return null;
+        }
       });
     } else {
       return null;
     }
   };
+
+  gotToHomePage = () => {
+    console.log("return");
+    this.props.history.push("/");
+  };
+
   render = () => {
+    const param = this.getParam();
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <Link className="navbar-brand" to="/">
@@ -38,11 +54,16 @@ class Nav extends React.Component {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarText">
-          <ul className="navbar-nav mr-auto">{this.getLinks()}</ul>
+          <ul className="navbar-nav mr-auto">{this.getLinks(param)}</ul>
+          {param ? (
+            <div className="return" onClick={this.gotToHomePage}></div>
+          ) : (
+            ""
+          )}
         </div>
       </nav>
     );
   };
 }
 
-export default Nav;
+export default withRouter(Nav);
